0e662c83d5457bb4b6ebae1f2817af6c37c2e43a
Matias-Galvan <matiasgalvan90@gmail.com>
2023-09-18 16:57:40 -0300

    Conexion exitosa con Firebase

---
+++ TEAyudo/Controllers/ChatController.cs
@@ -0,0 +1,41 @@
+﻿using Infraestructure.Connections;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TEAyudo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : Controller
+    {
+        Connection conection = new Connection();
+        [HttpPost]
+        public void setData()
+        {
+            
+           var data = new userChatDTO
+            {
+                userChatID = 1,
+                FotoPerfil = "foto1.jpg",
+                NombreUsuario = "Matias"
+            
+            };
+            try 
+            {
+                var SetData = conection.client.Set("Usuarios/" + "Nombre", data);
+            }
+            catch (Exception ex) 
+            {
+
+            }
+        }
+        
+
+    }
+
+    
+}
---
+++ TEAyudo/DTO/userChatDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class userChatDTO
+    {
+        public string NombreUsuario { get; set; }
+        public string FotoPerfil { get; set; }
+        public int userChatID {get; set; }
+
+    }
+}
